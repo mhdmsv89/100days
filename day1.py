57ca561c21759feb6ba3a6dee86552b9b2fb7414
Admin <mousavi.mehdi89@gmail.com>
2023-07-28 00:17:56 +0330

    change var band name generator

--- day1.py
+++ day1.py
@@ -1,9 +1,4 @@
-a = input("a:")
-b = input("b:")
-
-c=a
-a=b
-b=c
-
-print("a=" + a)
-print("b=" + b)
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
