34741b81cd27dcfed89a56894284001d98aa5027
Admin <mousavi.mehdi89@gmail.com>
2023-07-28 13:48:06 +0330

    bmi

--- day2.py
+++ day2.py
@@ -1,7 +1,10 @@
-#PEMDAS
-#Parentheses ()
-#Exponents **
-#Multiplication *
-#Division /
-#Addition +
-#Subtraction -
+w = input("What is your weight in kilogram?")
+h = input("What is your height in Meter")
+
+w_float = float(w)
+h_float = float(h)
+
+bmi = w_float / (h_float**2)
+result = str(bmi)
+
+print("Your BMI is " + result )
