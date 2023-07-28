2b96a3167c53ba69514962e921c9e12d4f43e5ee
Admin <mousavi.mehdi89@gmail.com>
2023-07-28 13:23:12 +0330

    pemdas

--- day2.py
+++ day2.py
@@ -1,8 +1,7 @@
-a = input("Insert a two digit number!")
-
-b = int(a[0])
-
-c = int(a[1])
-
-print(b+c)
-
+#PEMDAS
+#Parentheses ()
+#Exponents **
+#Multiplication *
+#Division /
+#Addition +
+#Subtraction -
