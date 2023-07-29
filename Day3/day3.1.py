ec9cd55eb65958a8a833c7fa5d8137efb175f175
Admin <mousavi.mehdi89@gmail.com>
2023-07-29 16:26:23 +0330

    photo_taken_added

--- Day3/day3.1.py
+++ Day3/day3.1.py
@@ -1,13 +1,21 @@
 print("Welcome to the roller coaster")
 h = int(input("Please insert your height in centimeter"))
+bill = 0
 if h >= 120:
     print("You can ride the roller coaster")
     age = int(input("Please insert your age"))
     if age < 12:
+        bill = 5
         print("Your ticket cost 5$")
     elif 12 <= age <= 18:
+        bill = 7
         print("Your ticket cost 7$")
     else:
+        bill = 12
         print("Your ticket cost 12$")
+    photo = input("Do you want a photo taken ? y or n")
+    if photo == "y":
+        bill += 3
+    print(f"Your final bill is {bill}")
 else:
     print("Sorry... you have to grow taller before you can ride")
