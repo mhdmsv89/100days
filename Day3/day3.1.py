685294f00524ecec5207a66998148ae6a5c2b56b
Admin <mousavi.mehdi89@gmail.com>
2023-07-29 14:45:59 +0330

    if_else_even_odd_checker elif_age

--- Day3/day3.1.py
+++ Day3/day3.1.py
@@ -2,5 +2,12 @@ print("Welcome to the roller coaster")
 h = int(input("Please insert your height in centimeter"))
 if h >= 120:
     print("You can ride the roller coaster")
+    age = int(input("Please insert your age"))
+    if age < 12:
+        print("Your ticket cost 5$")
+    elif 12 <= age <= 18:
+        print("Your ticket cost 7$")
+    else:
+        print("Your ticket cost 12$")
 else:
     print("Sorry... you have to grow taller before you can ride")
