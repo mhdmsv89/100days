e69e876191a53785111dff4ff56a6e8f54c2df24
Admin <mousavi.mehdi89@gmail.com>
2023-07-29 14:15:34 +0330

    if_else_height_checker

---
+++ Day3/day3.1.py
@@ -0,0 +1,6 @@
+print("Welcome to the roller coaster")
+h = int(input("Please insert your height in centimeter"))
+if h >= 120:
+    print("You can ride the roller coaster")
+else:
+    print("Sorry... you have to grow taller before you can ride")
