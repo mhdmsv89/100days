c8a582ac695e1cd7264ac614435fa246e64e277b
Admin <mousavi.mehdi89@gmail.com>
2023-07-29 15:13:01 +0330

    bmi_calc

--- Day3/day3.3.py
+++ Day3/day3.3.py
@@ -5,11 +5,11 @@ bmi = round( w / (h**2),1)
 
 if bmi < 18.5:
     print(f"Your BMI number is {bmi}, You are underweight")
-elif 18.5 <= bmi <= 25:
+elif bmi < 25:
     print(f"Your BMI number is {bmi}, You are normal weight")
-elif 25 < bmi <= 30:
+elif bmi < 30:
     print(f"Your BMI number is {bmi}, You are overweight")
-elif 30 < bmi <= 35:
+elif bmi < 35:
     print(f"Your BMI number is {bmi}, You are obese")
 else:
     print(f"Your BMI number is {bmi}, You are clinically obese")
