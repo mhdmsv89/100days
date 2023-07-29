b75ee92bcb9729d2c7279e3c9cd30baea366d767
Admin <mousavi.mehdi89@gmail.com>
2023-07-29 19:26:48 +0330

    l_calc

--- Day3/day3.6.py
+++ Day3/day3.6.py
@@ -1,5 +1,5 @@
 print("Welcome to Love Calculator")
-name1 = input("What is your name \n")
+name1 = input("What is your name ? \n")
 name2 = input("What is their name ? \n")
 lower_name1 = name1.lower()
 lower_name2 = name2.lower()
