e8c33d6086aee1a6b147d37a49bb583ca8867c08
Admin <mousavi.mehdi89@gmail.com>
2023-07-30 19:47:28 +0330

    list_random

---
+++ Day4/day4.2.py
@@ -0,0 +1,7 @@
+names_string = input("give me everybody's names, separated by a comma.")
+names = names_string.split (", ")
+names_list = len(names)
+import random
+random_int = random.randint(0,names_list)
+random_name = names[random_int]
+print(f"{random_name} must pay the bill")
