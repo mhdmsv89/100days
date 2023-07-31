5b1bd765e8400ae565a349e465745a185e108de0
Admin <mousavi.mehdi89@gmail.com>
2023-07-31 20:32:33 +0330

    even_sum

---
+++ Day5/day5.4.py
@@ -0,0 +1,12 @@
+even = 0
+sum_even = 0
+for n in range (1,101):
+    if n % 2 ==0:
+        even = n
+        sum_even +=even
+print(sum_even)
+
+total = 0
+for number in range(2, 101, 2):
+    total+=number
+print(total)
