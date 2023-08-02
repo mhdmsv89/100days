195892ab11437782b3b27d24860791f4f21548e3
Admin <mousavi.mehdi89@gmail.com>
2023-08-02 19:54:29 +0330

    hangman_1

---
+++ Day7/day7.1.py
@@ -0,0 +1,9 @@
+import random
+word_list = ["ardvark", "baboon","camel"]
+chosen_word = random.choice(word_list)
+guess_letter = input("Guess a letter").lower()
+for letter in chosen_word :
+    if letter == guess_letter:
+        print("Right")
+    else:
+        print("Wrong")
