92a394a2e5a97a6a7d9ad91711d7dfdc482d0d92
Admin <mousavi.mehdi89@gmail.com>
2023-08-03 11:05:21 +0330

    hangman_5

--- Day7/day7.5.py
+++ Day7/day7.5.py
@@ -64,7 +64,7 @@ display = []
 
 for letter in chosen_word:
     display += "_"
-print(display)
+print(f"{''.join(display)}")
 
 end_of_game = False
 
@@ -73,18 +73,16 @@ while not end_of_game:
 
     for position in range(len(chosen_word)):
         letter = chosen_word[position]
+
+        if guess_letter not in chosen_word:
+            lives -= 1
+            if lives == 0:
+                end_of_game = True
+                print("You Lose")
         if letter == guess_letter:
             display[position] = letter
-    if "_" not in display:
-        end_of_game = True
-        print("You Win")
-    print(f"{''.join(display)}")
+            print(f"{''.join(display)}")
 
-    if guess_letter not in chosen_word:
-        lives -= 1
-        if lives == 0:
-            end_of_game= True
-            print("You Lose")
-
-
-
+        if "_" not in display:
+            end_of_game = True
+            print("You Win")
