7d9012fcce97470534f0668780a8e4796f8afa7c
Admin <mousavi.mehdi89@gmail.com>
2023-08-06 20:53:10 +0330

    cipher_5

--- Day8/day8.4.py
+++ Day8/day8.4.py
@@ -1,7 +1,4 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
 # def cipher (text_i , shift_i):
 #     cipher_text =""
@@ -21,13 +18,27 @@ def cesar (start_text, shift_amount, cipher_direction):
     end_text = ""
     if cipher_direction == "decode":
         shift_amount *= -1
-    for letter in start_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        end_text += alphabet[new_position]
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text +=char
     print(f"The {cipher_direction}d text is {end_text}")
 
-cesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+    cesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    result = input("Type 'yes' if you want to go again, otherwise type 'no'.\n")
+    if result =="no":
+        should_continue = False
+        print("Good bye")
+
 
 
 # def encrypt (plain_text, shift_amount):
