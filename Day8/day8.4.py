27c885809d3f43d9477211c08d7b6451c32c16f3
Admin <mousavi.mehdi89@gmail.com>
2023-08-06 19:20:32 +0330

    cipher_2

--- Day8/day8.4.py
+++ Day8/day8.4.py
@@ -3,6 +3,7 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
+
 def encrypt (plain_text, shift_amount):
     cipher_text = ""
     for letter in plain_text:
@@ -11,16 +12,17 @@ def encrypt (plain_text, shift_amount):
         new_letter = alphabet[new_position]
         cipher_text +=new_letter
     print(f"The encoded message is {cipher_text}")
-encrypt(plain_text=text, shift_amount=shift)
 
-def decode (plain_text, shift_amount):
-    decipherd_text = ""
-    for letter in plain_text:
+def decode (cipher_text, shift_amount):
+    plain_text = ""
+    for letter in cipher_text:
         position = alphabet.index(letter)
         new_position = position - shift_amount
         new_letter = alphabet[new_position]
-        decipherd_text += new_letter
-    print(f"The decoded message is {decipherd_text}")
+        plain_text += new_letter
+    print(f"The decoded message is {plain_text}")
 
-
-decode(plain_text=text, shift_amount=shift)
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)
+elif direction == "decode":
+    decode(cipher_text=text, shift_amount=shift)
