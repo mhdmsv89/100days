11d9b1ef6cc6789d15a17a17c09d4866e70796d7
Admin <mousavi.mehdi89@gmail.com>
2023-08-08 12:01:01 +0330

    dictionary_bidding

---
+++ Day9/day9.3.py
@@ -0,0 +1,23 @@
+bid = {}
+bidding_finished = False
+
+def find_highest_bid (bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+while not bidding_finished:
+    name = input("What is your name?")
+    price = int(input("What is your bud? $"))
+    bid[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no'.")
+    if should_continue == "no":
+        bidding_finished = True
+        find_highest_bid(bid)
+    elif should_continue == "yes":
+        bidding_finished = False
